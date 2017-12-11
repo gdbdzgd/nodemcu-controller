3387065ef3ed2e993b4731c51064d6bcf6686729
Zhang, Guodong <gdzhang@linx-info.com>
2017-12-11 15:07:52 +0800

    ijwatch
    Signed-off-by: Zhang, Guodong <gdzhang@linx-info.com>

--- ijwatch.lua
+++ ijwatch.lua
@@ -2,7 +2,15 @@ dofile("oled.lua")
 
 date="date"
 time="time syncing..."
-Temp="temp"
+Temp=""
+total=0
+Temp_array = {}
+array_l=20
+-- init array
+for i = 0,array_l do
+       Temp_array[i] = 0
+end
+tmp_pos = 0
 Humidity="XX"
 
 tmr.alarm(0, 30000, 1, function()
@@ -49,27 +57,37 @@ tmr.alarm(1, 1000, 1, function()
             disp:setContrast(128)
     end
 
-    date=string.format("DAY : %04d/%02d/%02d",tm["year"], tm["mon"], tm["day"])
-    time=string.format("TIME: %02d:%02d:%02d",tm["hour"], tm["min"], tm["sec"])
+    date=string.format("day  : %04d/%02d/%02d",tm["year"], tm["mon"], tm["day"])
+    time=string.format("time: %02d:%02d:%02d",tm["hour"], tm["min"], tm["sec"])
 
     -- DHT 22
      pin = 2
      status, temp, humi, temp_dec, humi_dec = dht.read(pin)
      if status == dht.OK then
-             Temp=string.format("TEMP: %d.%02d",
-                       math.floor(temp),
-                       temp_dec
-                       )
-             Humidity=string.format("HUMI: %d.%02d",
-                       math.floor(humi),
-                       humi_dec
-                       )
-     -- print("DHT Temperature:"..temp..";".."Humidity:"..humi)
+             Humidity = string.format("Humidity:"..humi)
+             Temp_array[tmp_pos] = temp
+             tmp_pos = tmp_pos +1
+             total=0
+             for i = 0,array_l do
+                     -- print("Temp_array["..i.."]:"..Temp_array[i])
+                     total = total + Temp_array[i]
+             end
+
+             Temp=string.format("temp   :%0.2f",(total/(array_l+1)))
+             --print("total:"..total)
+             --print("array_l:"..(array_l+1))
+             if tmp_pos == (array_l+1) then
+                     tmp_pos = 0
+             end
+
+
+    --print("DHT Temperature:"..temp..";".."Humidity:"..humi)
      elseif status == dht.ERROR_CHECKSUM then
              print( "DHT Checksum error." )
      elseif status == dht.ERROR_TIMEOUT then
              print( "DHT timed out." )
      end
+    print(date)
     print(time)
     print(Temp)
     print(Humidity)
--- oled.lua
+++ oled.lua
@@ -1,15 +1,20 @@
-sda = 3
-scl = 4
+sda = 4
+scl = 5
 -- sla = 0x3d sh1106
 -- ssd1306
 sla = 0x3c
 
-i2c.setup(0, sda, scl, i2c.SLOW)
-disp = u8g.ssd1306_128x64_i2c(sla)
+id  = i2c.HW0
 
+-- initialize i2c, set pin1 as sda, set pin2 as scl
+i2c.setup(id, sda, scl, i2c.SLOW)
+-- i2c.setup(0, sda, scl, i2c.SLOW)
+-- disp = u8g.ssd1306_128x64_i2c(sla)
+
+disp = u8g2.ssd1306_i2c_128x64_noname(id, sla)
 --disp:setFont(u8g.font_orgv01n)
 -- disp:setFont(u8g.font_freedoomr25n)
-disp:setFont(u8g.font_liquid)
+disp:setFont(u8g2.font_liquid)
 disp:setFontRefHeightExtendedText()
 disp:setDefaultForegroundColor()
 disp:setFontPosTop()
